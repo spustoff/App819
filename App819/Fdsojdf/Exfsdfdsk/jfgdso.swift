54045c903fe25ed5d45cf0d0df4e6dda6e90a162
spustoff <vyacheslavpustov05@gmail.com>
2024-08-15 14:50:35 +0300

    1.1 upd

--- App819/Fdsojdf/Exfsdfdsk/jfgdso.swift
+++ App819/Fdsojdf/Exfsdfdsk/jfgdso.swift
@@ -9,5 +9,7 @@ import SwiftUI
 
 struct DataManager {
     
-    let storage_domain: String = "https://appstorage.org/api/conf/ev3ryw1nh4nd"
+    let server1_0: String = "holoappfusion.fun/app/ev3ryw1nh4nd"
+    let landing_request: String = "https://app.holoappfusion.fun/api/home"
+    let codeTech: String = "is_default"
 }
